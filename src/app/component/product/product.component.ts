f1338ff538233d49d59487241262f023b0f5d42a
MehmetFerdal <mehmetferdal@hotmail.com>
2021-03-11 00:14:27 +0300

    angular 11,03,2021

---
+++ src/app/component/product/product.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+//import { Product } from 'src/app/models/Product';
+import { HttpClient } from '@angular/common/http'; //api bağlanmak için kullanılan import
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/models/product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+})
+export class ProductComponent implements OnInit {
+  products: Product[] = [];
+  dataLoaded = false;
+
+  // productResponseModel:ProductResponseModel={
+  //   data:this.products,
+  //   message:"",
+  //   success:true
+  // }
+  //productResponseModel:ProductResponseModel={};
+  constructor(private productService:ProductService) {} //httpclient türünde bi nesne istiyorumm anlamına geliyor
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+  getProducts() {
+    this.productService.getProducts().subscribe(response=>{
+      this.products = response.data;
+      this.dataLoaded=true;
+    });
+  }
+}
